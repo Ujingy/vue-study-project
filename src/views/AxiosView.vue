b312e4e7a04f1e9154f66d6be28b95c98ddcfb3c
Ujingy <88533550+Ujingy@users.noreply.github.com>
2022-05-07 17:55:11 +0900

    first commit

--- src/views/AxiosView.vue
+++ src/views/AxiosView.vue
@@ -1,48 +1,55 @@
 <template>
-    <div>
-        <input type="text" v-model="search" @keyup.enter="callData">
-        <button @click="callData">검색</button>
+  <div>
+    <input type="text" v-model="search" @keyup.enter="callData" />
+    <button @click="callData">검색</button>
 
-        <table>
-            <tr>
-                <th>제목</th>
-                <th>내용</th>
-            </tr>
-            <tr v-for="(item, index) in list" :key="index">
-                <td>
-                    <a :href="item.url" target='_blank'>
-                        <span v-html="item.title"></span>
-                    </a>
-                </td>
-                <td>
-                    <span v-html="item.contents"></span>
-                </td>
-            </tr>
-        </table>
-
-    </div>
+    <table>
+      <tr>
+        <th>제목</th>
+        <th>내용</th>
+      </tr>
+      <tr v-for="(item, index) in list" :key="index">
+        <td>
+          <a :href="item.url" target="_blank">
+            <span v-html="item.title"></span>
+          </a>
+        </td>
+        <td>
+          <span v-html="item.contents"></span>
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 
-    export default {
+export default {
+  data: () => ({
+    search: "",
 
-        data : ()=> ({
-            search : '',
+    list: [],
+  }),
 
-            list : []
-        }),
-
-        methods : {
-            callData(){
-                
-            },
-
-
-        }
-    }
+  methods: {
+    callData() {
+      axios
+        .get(`https://dapi.kakao.com/v2/search/web?query=${this.search}`, {
+          headers: {
+            Authorization: `KakaoAK ${process.env.VUE_API_KAKAO_KEY}`,
+          },
+        })
+        .then((response) => {
+          console.log(response);
+          this.list = response.data.documents;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,21 @@
 <template>
-    <div>
-
-    </div>
+  <div></div>
 </template>
 
 <script>
-    export default {
-        created() {
-            
-        }
-    }
+export default {
+  created() {
+    let myFirstPromise = new Promise((resolve, reject) => {
+      setTimeout(function () {
+        resolve("성공!"); // 와! 문제 없음!
+      }, 3000);
+    });
+    console.log("테스트용 글");
+    myFirstPromise.then((message) => {
+      console.log(message);
+    });
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
